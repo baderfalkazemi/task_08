94371ab7dc38473f82019a1b3ba1cad991d31c54
baderfalkazemi <bader.alkazemi@gmail.com>
2018-12-03 17:23:33 +0300

    submit task8

--- restaurants/models.py
+++ restaurants/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length=120)
     description = models.TextField()
+    logo = models.ImageField(null=True, blank=True)
     opening_time = models.TimeField()
     closing_time = models.TimeField()
